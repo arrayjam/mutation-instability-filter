f8c806846d1589f89999b72bf4fa4c7fa5b9e3be
Savant Krishna <savant.2020@gmail.com>
2014-10-25 13:51:15 +1100

    finding start sequence number

---
+++ start_sequence_finder.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+
+# Usage: ./start_sequence_finder.py [istable|eris] [json-file.json]
+#
+import json,sys
+
+
+jfile_name = ""
+
+def notAmino (s):
+  if "Sequence as provided" in s:
+    return True
+  return False
+
+def getNum (s):
+  try:
+    num = int(s[4:6])
+    #print num
+    return num
+  except:
+    #print "No num"
+    return -1
+
+def readFile ():
+  global jfile_name
+  jfile = open (jfile_name, 'r');
+  data = json.load (jfile)
+  chainid = "D"
+  rawsequence = data["byChain"][chainid]["img"]["mapData"]["sequence"]
+  sequence  = []
+  for row in rawsequence:
+    sequence = sequence + row
+  #print "sequence length:",len(sequence)
+  iter_num = 0
+  for amino in sequence:
+    desc = amino["t"]
+    if notAmino (desc):
+      continue
+    num = getNum (desc)
+    if num is not -1:
+      #print amino
+      return num-iter_num, num
+      break
+    iter_num = iter_num + 1
+
+def getStartIstable ():
+  start, latestart = readFile ()
+  return start
+
+def getStartEris ():
+  start, latestart = readFile ()
+  return latestart
+
+if __name__=='__main__':
+  try:
+    try:
+      jfile_name = sys.argv [2]
+    except IndexError:
+      jfile_name = "2OCJ.json"
+    if sys.argv[1] ==  'istable':
+      print getStartIstable ()
+    elif sys.argv[1] == 'eris':
+      print getStartEris ()
+    else:
+      print "Usage: ./start_sequence_finder.py [istable|eris] [json-file.json]"
+  except IndexError:
+    print getStartIstable ()
+
+  #print 'Istable', getStartIstable ()
+  #print 'Eris', getStartEris ()
