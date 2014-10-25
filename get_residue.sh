ce8ac37c54aabe1403fd47e412a63f4b20097132
Savant Krishna <savant.2020@gmail.com>
2014-10-25 12:44:09 +1100

    argument as protein pdb id and save to file

--- get_residue.sh
+++ get_residue.sh
@@ -1,1 +1,7 @@
-curl "http://www.rcsb.org/pdb/explore/remediatedSequence.do?structureId=2OCJ&bionumber=1" | grep SequencePage | sed 's/^.*SequencePage(//' | sed 's/);$//'
+if [ "x$1" = "x" ]; then
+  protein_name="2OCJ"
+else
+  protein_name=$1
+fi
+json_name="$protein_name.json"
+curl "http://www.rcsb.org/pdb/explore/remediatedSequence.do?structureId=$protein_name&bionumber=1" | grep SequencePage | sed 's/^.*SequencePage(//' | sed 's/);$//' > $json_name
