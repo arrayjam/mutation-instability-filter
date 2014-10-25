3ca6d5506aa3308bcd679866372ef242e7ac3a80
Yuri Feldman <yuri@tbqh.net>
2014-10-25 13:04:44 +1100

    Residue JSON parsing

--- get_residue.sh
+++ get_residue.sh
@@ -4,4 +4,7 @@ else
   protein_name=$1
 fi
 json_name="$protein_name.json"
+
 curl "http://www.rcsb.org/pdb/explore/remediatedSequence.do?structureId=$protein_name&bionumber=1" | grep SequencePage | sed 's/^.*SequencePage(//' | sed 's/);$//' > $json_name
+
+cat $json_name | jq "(.byChain.D.img.mapData.sequence | add)[].t"
