b3461f866e06754dafd655ae32382fd87a652738
Yuri Feldman <yuri@tbqh.net>
2014-12-11 03:13:40 +1100

    Add foreman management to capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -46,3 +46,28 @@ namespace :deploy do
   end
 
 end
+
+after "deploy:update", "foreman:export"
+after "deploy:update", "foreman:restart"
+
+namespace :foreman do
+  desc "Export the Procfile to Ubuntu's upstart scripts"
+  task :export, roles: :app do
+    run "cd #{release_path} && sudo bundle exec foreman export upstart /etc/init -a #{application} -u #{user} -l #{shared_path}/log"
+  end
+
+  desc "Start the application services"
+  task :start, roles: :app do
+    sudo "start #{application}"
+  end
+
+  desc "Stop the application services"
+  task :stop, roles: :app do
+    sudo "stop #{application}"
+  end
+
+  desc "Restart the application services"
+  task :restart, roles: :app do
+    run "sudo start #{application} || sudo restart #{application}"
+  end
+end
